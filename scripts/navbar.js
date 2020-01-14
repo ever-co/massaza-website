9a89bed4740975de5f0ceb6321dc491f4b71bc42
Dimana <dimsi123.dc@gmail.com>
2020-01-14 20:13:38 +0200

    Home page

---
+++ scripts/navbar.js
@@ -0,0 +1,26 @@
+
+let miniMenu = document.getElementById("mini-menu");
+let navbar = document.getElementById("navbar");
+let hiddenMenu = document.getElementById("hidden-menu");
+let logoContainer = document.getElementById("logo");
+let isOpen = false;
+
+miniMenu.addEventListener("click", function(e){
+    e.preventDefault();
+    e.stopPropagation();
+    console.log(logoContainer.offsetWidth);
+    if(!isOpen){
+        navbar.classList.remove("close");
+        navbar.classList.add("open");
+        hiddenMenu.style.width = `${logoContainer.offsetWidth }px`;
+        hiddenMenu.style.transform  = "translate(0, 0)";
+        isOpen= true;
+    }else if(isOpen){
+        navbar.classList.remove("open");
+        navbar.classList.add("close");
+        hiddenMenu.style.transform  = "translate(-600px, 0)";
+        console.log(hiddenMenu.style.width)
+        isOpen = false;
+    }
+})
+
